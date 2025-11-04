beb6322d5f6ba927339ae481e8955c5c874ee8c6
eleandrovanelli <eleandro.vanelli@ciss.com.br>
2025-11-04 20:57:24 -0300

    Criado módulo de Categorias

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const categoryController = require('../controller/category');
 const authMiddleware = require('../middlewares/auth');
-const adminMiddleware = require('../middlewares/admin');
 
 const router = express.Router();
 
 // Rotas públicas ou com autenticação básica
 router.get('/', authMiddleware, categoryController.getAllCategories);
 router.get('/:id', authMiddleware, categoryController.getCategoryById);
-
-// Rotas que requerem admin
-router.post('/', authMiddleware, adminMiddleware, categoryController.createCategory);
-router.put('/:id', authMiddleware, adminMiddleware, categoryController.updateCategory);
-router.delete('/:id', authMiddleware, adminMiddleware, categoryController.deleteCategory);
+router.post('/', authMiddleware, categoryController.createCategory);
+router.put('/:id', authMiddleware, categoryController.updateCategory);
+router.delete('/:id', authMiddleware, categoryController.deleteCategory);
 
 module.exports = router;
 
